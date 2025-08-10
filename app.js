52bcc53434e8c0d4292348e150ca1e65437ab270
BastianRivas <brivass1@uft.edu>
2025-08-10 12:33:54 -0400

    Crea funcion agregarAmigo y limpiarCaja

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
-const campoTextoAmigo= document.getElementById('amigo')
-function agregarAmigo(amigo){
-    amigos.push(amigo)
+let amigos = [];
+
+const campoTextoAmigo= document.getElementById('amigo');
+
+function limpiarCaja(documentoLimpiar) {
+    documentoLimpiar.value = '';
+}
+
+function agregarAmigo(){
+    console.log('Esto es lo que tiene el campo amigo: '+ campoTextoAmigo.value);
+    const amigoAgregar = campoTextoAmigo.value;
+    if(!amigoAgregar)
+    {
+        alert('El campo no puede estar vacio. Por favor, inserte un nombre.'); 
+        return;
+    }    
+    amigos.push(amigoAgregar);
+    limpiarCaja(campoTextoAmigo);
 }
